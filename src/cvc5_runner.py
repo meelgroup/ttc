179c99f5cd404ef92c9c8c338c255166a2915134
Arijit Shaw <if.arijit@gmail.com>
2024-06-02 12:33:11 -0400

    two-pass system to create latte constraints, cvc5runner as class not running

--- src/cvc5_runner.py
+++ src/cvc5_runner.py
@@ -1,43 +1,47 @@
+import os
 import subprocess
 from .literal_mapping import LiteralMapping
 from .utils import log
 from .global_storage import gbl
 
+class CVC5Runner:
+    def __init__(self, smt_file):
+        self.smt_file = smt_file
 
-def run_cvc5_on_smt_file(smt_file):
-    log(f"Running cvc5 on {smt_file}", 1)
-    # RUN my version of CVC5 which outputs mapping
-    result = subprocess.run(["cvc5", smt_file], capture_output=True, text=True)
-    if result.returncode != 0:
-        raise RuntimeError(f"cvc5 error: {result.stderr}")
-    log(f"cvc5 output: {result.stdout}", 4)
-    return result.stdout
+    def run_cvc5_on_smt_file(self):
+        log(f"Running cvc5 on {self.smt_file}", 1)
+        # RUN my version of CVC5 which outputs mapping
+        result = subprocess.run(["cvc5", self.smt_file],
+                                capture_output=True, text=True)
+        if result.returncode != 0:
+            raise RuntimeError(f"cvc5 error: {result.stderr}")
+        log(f"cvc5 output: {result.stdout}", 4)
+        return result.stdout
 
-
-def parse_cvc5_output(output):
-    log("Parsing cvc5 output", 1)
-    mapping = LiteralMapping()
-    cnf_lines = []
-    for line in output.splitlines():
-        if line.startswith('c '):
-            parts = line.split(':')
-            # TODO assert that ~ could be skipped
-            if '~' in parts[0]:
+    def parse_cvc5_output(self, output):
+        log("Parsing cvc5 output", 1)
+        mapping = LiteralMapping()
+        cnf_lines = []
+        for line in output.splitlines():
+            if line.startswith('c '):
+                parts = line.split(':')
+                # TODO assert that ~ could be skipped
+                if '~' in parts[0]:
+                    continue
+                literal = int(parts[0][2:])
+                inequality = parts[1].strip()
+                if '~' in parts[0]:
+                    literal = -literal
+                mapping.add_mapping(literal, inequality)
+            elif line.startswith('unsat'):
                 continue
-            literal = int(parts[0][2:])
-            inequality = parts[1].strip()
-            if '~' in parts[0]:
-                literal = -literal
-            mapping.add_mapping(literal, inequality)
-        elif line.startswith('unsat'):
-            continue
-        elif line.startswith('p cnf') or (not line.startswith('c')):
-            cnf_lines.append(line)
-    cnf_content = "\n".join(cnf_lines)
-    log(f"Parsed CNF content: \n{cnf_content} \n cnf end", 3)
-    cnf_file_name = gbl.arg.smt_file.split("/")[-1].replace(".smt2", ".cnf")
-    with open(cnf_file_name, 'w') as f:
-        f.write(cnf_content)
-    log(f"created CNF file: {cnf_file_name}")
-    log(f"CNF literal to atoms Mapping: {mapping}", 2)
-    return mapping, cnf_file_name
+            elif line.startswith('p cnf') or (not line.startswith('c')):
+                cnf_lines.append(line)
+        cnf_content = "\n".join(cnf_lines)
+        log(f"Parsed CNF content: \n{cnf_content} \n cnf end", 3)
+        cnf_file_name = self.smt_file.split("/")[-1].replace(".smt2", ".cnf")
+        with open(cnf_file_name, 'w') as f:
+            f.write(cnf_content)
+        log(f"created CNF file: {cnf_file_name}")
+        log(f"CNF literal to atoms Mapping: {mapping}", 2)
+        return mapping, cnf_file_name
--- src/literal_mapping.py
+++ src/literal_mapping.py
@@ -1,5 +1,7 @@
 from lark import Lark, Transformer, v_args
 from .utils import log
+import pandas as pd
+
 # TODO sum can be of multiple products
 # TODO what happens for free literals
 # TODO check well for correctness of the converted constraints
@@ -22,6 +24,58 @@ grammar = """
     %import common.WS
     %ignore WS
 """
+
+
+class VariableCreator(Transformer):
+    def __init__(self):
+        self.variables = set()
+
+    def inequality(self, *items):
+        log(f"inequalityx: {items}", 4)
+        return {
+            "type": "inequality",
+            "operator": ">=",
+            "left": items[0][0],
+            # Convert the number to int here
+            "right": int(items[0][1])
+        }
+
+    def sum(self, *items):
+        return {
+            "type": "sum",
+            "terms": list(items)
+        }
+
+    def product(self, *items):
+        log(f"product: {items}", 4)
+        # append to set variables the item[0][1]
+        self.variables.add(items[0][1])
+
+        return {
+            "type": "product",
+            # Convert the coefficient to int
+            "coefficient": int(items[0][0]),
+            "variable": items[0][1]
+        }
+
+    def number(self, item):
+        log(f"number: {item}", 4)
+        token = item[0]
+        value = int(token.value)
+        sign = -1 if value < 0 else 1
+        log(f"signed_number: {sign * value}", 4)
+        return sign * value
+
+    def signed_number(self, items):
+        # items is a list containing one token, e.g., [Token('SIGNED_NUMBER', '-2')]
+        token = items[0]
+        value = int(token.value)
+        sign = -1 if value < 0 else 1
+        log(f"signed_number: {sign * value}", 4)
+        return sign * value
+
+    def VAR(self, item):
+        return str(item)
 
 
 class ExpressionTransformer(Transformer):
@@ -74,6 +128,10 @@ class ExpressionTransformer(Transformer):
 class LiteralMapping:
     def __init__(self):
         self.mapping = {}
+        # create empty dataframe for constraints
+        self.constraint_matrix = pd.DataFrame()
+        self.variable_creator_parser = Lark(grammar, parser='lalr',
+                                            transformer=VariableCreator())
         self.parser = Lark(grammar, parser='lalr',
                            transformer=ExpressionTransformer())
 
@@ -90,6 +148,16 @@ class LiteralMapping:
             ineq = " ".join(map(str, [-c for c in coefficients]))
             neg_ineq = " ".join(map(str, [-c for c in negated_coefficients]))
             return ineq, neg_ineq
+
+    def create_constraint_matrix(self, inequalities):
+        self.variables = set()
+
+        for ineq in inequalities:
+            variable_set = self.variable_creator_parser.parse(ineq)
+            self.variables = self.variables.union(variable_set.variables)
+
+        # create empty dataframe with columns as variables, and number of inequalities many rows
+        self.constraint_matrix = pd.DataFrame(columns=self.variables, index=range(len(inequalities)))
 
     def add_mapping(self, literal, inequality):
         if literal in [0, 1]:
