64405c4afd0b547c9cb5b09a00ea2b7bcf226f27
Arijit Shaw <if.arijit@gmail.com>
2024-06-04 17:39:31 -0400

    remove unnecessary things

--- src/literal_mapping.py
+++ src/literal_mapping.py
@@ -93,13 +93,6 @@ class ExpressionTransformer(Transformer, list):
         # self.constraints = pd.DataFrame(0, columns=self.variables, index=range(1))
         self.constraints.loc[0, "const"] = - items[0][1].children[0]
         return self.constraints
-        # return {
-        #     "type": "inequality",
-        #     "operator": ">=",
-        #     "left": items[0][0],
-        #     # Convert the number to int here
-        #     "right": items[0][1]
-        # }
 
     def sum(self, *items):
         return {
@@ -143,9 +136,7 @@ class ExpressionTransformer(Transformer, list):
 
 class LiteralMapping:
     def __init__(self):
-        self.mapping = {}
         self.variables = set()
-        self.num_inequalities = 0
         # create empty dataframe for constraints
         self.constraint_matrix = pd.DataFrame()
         self.variable_creator_parser = Lark(grammar, parser='lalr',
@@ -178,35 +169,9 @@ class LiteralMapping:
         traverse(tree)
         return variables
 
-    def convert_to_latte(self, parsed_tree):
-        if parsed_tree["type"] == "inequality":
-            left = parsed_tree["left"]
-            right = parsed_tree["right"].children[0]
-            log(f"left: {left}, right: {right}", 4)
-            lhs = left["terms"][0]
-            coefficients = []
-            for term in lhs:
-                # get datatype of coefficient
-                # Check if coefficient is a list
-                if not isinstance(term["coefficient"], int):
-                    coefficient = term["coefficient"].children[0]
-                else:
-                    coefficient = term["coefficient"]
-                coefficients.append(coefficient)
-            # coefficients = [term["coefficient"].children[0] for term in lhs]
-            negated_coefficients = [-term for term in coefficients]
-
-            coefficients.insert(0, -right)
-            negated_coefficients.insert(0, right - 1)
-
-            ineq = " ".join(map(str, [c for c in coefficients]))
-            neg_ineq = " ".join(map(str, [c for c in negated_coefficients]))
-            return ineq, neg_ineq
-
     def add_variable_in_constraint_matrix(self, ineq):
         parsed_tree = self.variable_creator_parser.parse(ineq)
         self.variables = self.variables.union(self.get_variables(parsed_tree))
-        self.num_inequalities += 1
 
     def finalize_variable_matrix(self):
         self.variables = ["const"] + list(self.variables)
@@ -225,22 +190,19 @@ class LiteralMapping:
         return df
 
     def copy_constraint_matrix(self, constraint_line, literal):
-        # copy constraint line to constraint matrix
         constraint_line.index = [literal]
         self.constraint_matrix.loc[literal] = constraint_line.iloc[0]
 
     def add_mapping(self, literal, inequality):
         if literal in [0, 1]:
-            return  # Ignore literals 0 and 1
+            return
         log(f"now mapping literal: {literal}, inequality: {inequality}", 3)
         self.parser = Lark(grammar, parser='lalr',
                            transformer=ExpressionTransformer(self.variables))
         inequality_line = self.parser.parse(inequality)
         neg_inequality_line = self.negate_constraint(inequality_line)
-        # ineq, neg_ineq = self.convert_to_latte(parsed_tree)
         self.copy_constraint_matrix(inequality_line, literal)
         self.copy_constraint_matrix(neg_inequality_line, -literal)
-        # log(f"latte format: {ineq}\n \t neg: {neg_ineq}", 3)
 
         log(f"mapping now: {self.constraint_matrix}", 4)
 
@@ -248,7 +210,7 @@ class LiteralMapping:
         inequalities = []
         for lit in literals:
             if lit in [0, 1]:
-                continue  # Ignore literals 0 and 1
+                continue
             inequality = self.constraint_matrix.loc(lit)
             if inequality:
                 inequalities.append(inequality)
